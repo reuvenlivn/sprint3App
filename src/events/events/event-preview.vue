77c727be7a65db0cd98a5a818b301e7a2d3c8928
reuvenlivn <reuvenlivn@gmail.com>
2016-12-23 17:10:28 +0200

    support event details and move to edit

--- src/events/events/event-preview.vue
+++ src/events/events/event-preview.vue
@@ -1,13 +1,10 @@
 <template>
     <section :class="{selected: event.isSelected}">
         <div>{{event.name}}</div>
-        <div>{{event.venue.address_1}}</div>
-        <div>{{event.venue.address_2}}</div>
-        <div>{{event.venue.city}}</div>
+        <div>{{readableTime(event.time)}}</div>
         <div v-html=shortContent(event.description)></div>
         <!--<img :src="`src/assets/event/${event.id}.png`" alt="">-->
         <button @click.stop="deleteEvent">Delete</button>
-        <!--<button @click.stop="editEvent">Edit</button>-->
        <router-link :to="`/event/${event.id}/edit`">Edit</router-link>
     </section>
 </template>
@@ -38,7 +35,11 @@
 
             shortContent(longStr){
                  return longStr.substring(0, 200);
-            }      
+            },
+
+            readableTime(timestamp)  {
+                return (new Date(timestamp ));
+            } 
         }
     }
 </script>
@@ -50,5 +51,16 @@
     .selected {
         border: 1px solid darkorchid;
     }
-     
+
+    .thumbnail {
+    display: block;
+    padding: 4px;
+    margin-bottom: 20px;
+    line-height: 1.42857143;
+    background-color: #fff;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    -webkit-transition: border .2s ease-in-out;
+    transition: border .2s ease-in-out; 
+    }    
 </style>
